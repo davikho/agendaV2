e24bf4a551ce3fa8401d244bfbd5f0e8178cd60e
David Flores <davikho85@gmail.com>
2025-02-27 19:35:09 -0500

    con api

--- app/src/main/java/com/example/agendav2/utils/Constants.kt
+++ app/src/main/java/com/example/agendav2/utils/Constants.kt
@@ -1,5 +1,6 @@
 package com.example.agendav2.utils
 
 object Constants {
-    const val BASE_URL = "http://10.0.2.2:5000/"
+    //const val BASE_URL = "http://10.0.2.2:5000/"
+    const val BASE_URL = "https://apiaso.america3000.site/"
 }
